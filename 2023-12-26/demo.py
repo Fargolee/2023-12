adf8b347780c625f20a7bd25bc9d3399759fda6f
Lee <775977072@qq.com>
2023-12-26 23:02:19 +0800

    📝

---
+++ 2023-12-26/demo.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Author ：Lee fargolyh@gmail.com
+@Project ：2023-12 
+@File ：demo.py
+@Date ：2023/12/26 21:35 
+"""
+# from selenium import webdriver
+
+
+# # 设置chromedriver路径
+# # chromedriver_path = "/path/to/chromedriver"
+#
+# # 创建chrome选项
+# chrome_options = Options()
+# chrome_options.add_argument('--headless')
+#
+# # 创建webdriver实例
+# driver = webdriver.Chrome(options=chrome_options)
+#
+# # 进行其他webdriver操作
+
+# from selenium import webdriver
+#
+# driver = webdriver.Chrome()
+#
+# str1 = driver.capabilities['browserVersion']  # 查看chrome版本
+# str2 = driver.capabilities['chrome']['chromedriverVersion'].split(' ')[0]  # 查看python下的chromedriver版本
+# print(str1)
+# print(str2)
+
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+# # option 1
+# # driver = webdriver.Chrome(ChromeDriverManager().install())
+#
+# # option 2
+# options = webdriver.ChromeOptions()
+# options.add_argument("--headless")
+# driver = webdriver.Chrome(chrome_options=options)
+
+from selenium import webdriver
+
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
+
+# # 创建浏览器对象
+# driver = webdriver.Chrome()
+#
+# # 执行自动化操作
+# # ...
+#
+# # 程序即将结束时，询问是否关闭浏览器
+# choice = input("Do you want to close the browser? (Y/N) ")
+# if choice.lower() == "y":
+#     # 关闭浏览器
+#     driver.quit()
+# else:
+#     print("Browser will remain open.")
